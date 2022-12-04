1567bf7ad98ae2ea9d303d72117ae89712d0b130
Yulu Xu <xuyulu86@126.com>
2022-12-04 18:32:30 +0800

    add assets load

--- lib/ember_quest.dart
+++ lib/ember_quest.dart
@@ -1,3 +1,18 @@
 import 'package:flame/game.dart';
 
-class EmberQuest extends FlameGame {}
+class EmberQuest extends FlameGame {
+  EmberQuest();
+
+  @override
+  Future<void>? onLoad() async {
+    await images.loadAll([
+      'block.png',
+      'ember.png',
+      'ground.png',
+      'heart.png',
+      'heart_half.png',
+      'star.png',
+      'water_enemy.png',
+    ]);
+  }
+}
